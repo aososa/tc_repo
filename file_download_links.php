6b1ce23f80eac0f8de22bd98dc7c7dcd16506e6d
aososa <a.sosa@klab.com>
2014-10-14 10:13:28 +0800

    Add new file containing download links for PHP/MySQL setup

---
+++ file_download_links.php
@@ -0,0 +1,6 @@
+<a href="mysql-client-5.5_5.5.27-0ubuntu2_amd64.deb">Client</a><br>
+<a href="mysql-server-5.5_5.5.27-0ubuntu2_amd64.deb">Server</a><br>
+<a href="mysql-server-core-5.5_5.5.27-0ubuntu2_amd64.deb">Server Core</a><br>
+<br>
+<a href="php5_5.3.10-1ubuntu3.14_all.deb">PHP 5.5.3</a><br>
+
